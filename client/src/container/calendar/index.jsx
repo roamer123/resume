352fbd4898d8b78b9c91dde48bde6cad58a2b6d3
lulu <lulu27753@163.com>
2018-07-05 21:51:00 +0800

    +calendar

--- client/src/container/calendar/index.jsx
+++ client/src/container/calendar/index.jsx
@@ -1,10 +1,100 @@
 import React from 'react'
+import SearchFilter from 'component/search-filter';
+import { Button, Table } from 'components';
+import styles from './index.less'
 
 export default class Calendar extends React.Component {
   render () {
+    const filterDetail = {}
+    const columns = [{
+        title: 'Full Name',
+        width: 100,
+        dataIndex: 'name',
+        key: 'name',
+        fixed: 'left'
+      },
+      {
+        title: 'Age',
+        width: 100,
+        dataIndex: 'age',
+        key: 'age',
+        fixed: 'left'
+      },
+      {
+        title: 'Column 1',
+        dataIndex: 'address',
+        key: '1',
+        width: 150
+      },
+      {
+        title: 'Column 2',
+        dataIndex: 'address',
+        key: '2',
+        width: 150
+      },
+      {
+        title: 'Column 3',
+        dataIndex: 'address',
+        key: '3',
+        width: 150
+      },
+      {
+        title: 'Column 4',
+        dataIndex: 'address',
+        key: '4',
+        width: 150
+      },
+      {
+        title: 'Column 5',
+        dataIndex: 'address',
+        key: '5',
+        width: 150
+      },
+      {
+        title: 'Column 6',
+        dataIndex: 'address',
+        key: '6',
+        width: 150
+      },
+      {
+        title: 'Column 7',
+        dataIndex: 'address',
+        key: '7',
+        width: 150
+      },
+      {
+        title: 'Column 8',
+        dataIndex: 'address',
+        key: '8'
+      },
+      {
+        title: 'Action',
+        key: 'operation',
+        fixed: 'right',
+        width: 100,
+        render: () => <a href='javascript:;' > action </a>,
+      },
+    ];
+
+    const data = [];
+    for (let i = 0; i < 100; i++) {
+      data.push({
+        key: i,
+        name: `Edrward ${i}`,
+        age: 32,
+        address: `London Park no. ${i}`,
+      });
+    }
     return (
-      <div>
-        calendar
+      <div className={styles.calendar_list}>
+        <div className={styles.action_area}>
+          <SearchFilter columns={filterDetail} className={styles.search_filter} />
+          <div>
+            <Button>安排面试</Button>
+            <Button className={styles.sign_button}>批量签到</Button>
+          </div>
+        </div>
+        <Table className={styles.table} columns={columns} dataSource={data} scroll={{ x: 1500, y: 300 }} />
       </div>
     )
   }
