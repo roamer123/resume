78959f1206989d99e2440c51c33a6e240d478561
roamer123 <604070939@qq.com>
2018-07-09 17:15:21 +0800

    @sql

---
+++ docs/db/USER_INSERT.sql
@@ -0,0 +1,9 @@
+INSERT INTO `USER` VALUES 
+    ('1', 'admin', 'aaaaa888', 'ROLE_ADMIN', '', '', 'lulu@163.com', '13874982777', null, 'system', NOW(), 'system', NOW()),
+    ('2', 'hr001', 'aaaaa888', 'ROLE_HR', 'SUPPLIER_ORGANIZATION', 'SUPPLIER_WSHH', 'lulu@163.com', '13874982777', null, 'system', NOW(), 'system', NOW()),
+    ('3', 'hr002', 'aaaaa888', 'ROLE_HR', 'SUPPLIER_ORGANIZATION', 'SUPPLIER_WSHH', 'lulu@163.com', '13874982777', null, 'system', NOW(), 'system', NOW()),
+    ('3', 'hr003', 'aaaaa888', 'ROLE_HR', 'SUPPLIER_ORGANIZATION', 'SUPPLIER_ZR', 'lulu@163.com', '13874982777', null, 'system', NOW(), 'system', NOW()),
+    ('4', 'interviewer001', 'aaaaa888', 'NEED_ORGANIZATION', '0', 'NEED_ZGPA', 'lulu@163.com', '13874982777', null, 'system', NOW(), 'system', NOW()),
+    ('5', 'interviewer002', 'aaaaa888', 'NEED_ORGANIZATION', '0', 'NEED_ALBB', 'lulu@163.com', '13874982777', null, 'system', NOW(), 'system', NOW()),
+    ('6', 'principal001', 'aaaaa888', 'NEED_ORGANIZATION', '0', 'NEED_ZGPA', 'lulu@163.com', '13874982777', null, 'system', NOW(), 'system', NOW()),
+    ('7', 'hr0principal002', 'aaaaa888', 'NEED_ORGANIZATION', '0', 'NEED_ALBB', 'lulu@163.com', '13874982777', null, 'system', NOW(), 'system', NOW());
