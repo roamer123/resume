53f3a7869e41e4c188462b98ca1f9869b1388f54
lulu <lulu27753@163.com>
2018-08-14 09:56:57 +0800

    FilterStep组件更改

--- client/src/component/filter-step/index.jsx
+++ client/src/component/filter-step/index.jsx
@@ -1,24 +1,39 @@
 import React from 'react'
-import {liCreate} from 'utils/creator';
-import styles from './index.less'
+import classNames from 'classnames'
 
+import styles from './index.less'
+const VALUE = 'VALUE'
+const CODE = 'CODE'
 export default class FilterStep extends React.Component {
   render () {
     const {colums, data, handleStep, active} = this.props
     return (
-      <div className={styles.filter_step}>
         <ul className={styles.filter_group}>
           {
-              liCreate({
-                lis: colums,
-                liData: data,
-                handleClick: handleStep,
-                active,
-                addonAfter: '人'
-              })
+          colums.map((li, i) => {
+                const step = li[VALUE]
+                const code = li[CODE]
+                return (
+                  <li
+                    onClick={(e) => handleStep(e, step, li)}
+                    key={i}
+                 >
+                    <p
+                      className={classNames(styles.step, {active: code === active})}
+                     >
+                       <span className={classNames('mr8', styles.circle, {active_bg: code === active})} />
+                       {`${step}`}
+                     </p>
+                    <p className={classNames(styles.data, {active: code === active})}>{`${data[code] || 0}`}</p>
+                    <div className={styles.bow} />
+                  </li>
+
+                )
+              }
+            )
           }
         </ul>
-      </div>
     )
   }
 }
+
